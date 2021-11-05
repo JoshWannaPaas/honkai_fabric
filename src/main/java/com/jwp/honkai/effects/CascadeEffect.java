8e8cedbec43c709a0710625e98414718b66c58f8
JoshWannaPaas <paasjoshua@yahoo.com>
2021-11-04 19:46:37 -0700

    Update CascadeEffect.java
    Cleaned up

--- src/main/java/com/jwp/honkai/effects/CascadeEffect.java
+++ src/main/java/com/jwp/honkai/effects/CascadeEffect.java
@@ -1,14 +1,13 @@
 package com.jwp.honkai.effects;
 
 import com.jwp.honkai.damagesources.CascadeDamageSource;
+import com.jwp.honkai.registry.ModItems;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectType;
-import net.minecraft.entity.player.PlayerEntity;
 
 public class CascadeEffect extends StatusEffect {
-    private CascadeDamageSource CASCADING = new CascadeDamageSource();
-
+    private final CascadeDamageSource CASCADING = new CascadeDamageSource();
 
     public CascadeEffect() {
         super(StatusEffectType.HARMFUL, 0xFF0000);
@@ -16,8 +15,6 @@ public class CascadeEffect extends StatusEffect {
 
     @Override
     public boolean canApplyUpdateEffect(int duration, int amplifier){
-        //if holding honkai material item, true
-        //if not, false
         return true;
     }
 
